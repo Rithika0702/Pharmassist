05b94bc59fefe85abcc025988f8074c09a5d4abb
Rithika0702 <rithumahathi@gmail.com>
2024-10-28 19:55:43 +0530

    project initialized,database configuration is done

---
+++ src/main/java/com/jsp/pharmassist/PharmassitApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.pharmassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmassitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmassitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/pharmassist/PharmassitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.pharmassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmassitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
