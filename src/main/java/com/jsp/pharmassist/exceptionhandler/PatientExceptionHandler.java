68c6f656c12d9c1388892c96494bcbd48c46545f
Rithika0702 <rithumahathi@gmail.com>
2024-11-05 10:36:46 +0530

    Patient Update feature is added

---
+++ src/main/java/com/jsp/pharmassist/exceptionhandler/PatientExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.jsp.pharmassist.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.jsp.pharmassist.exception.PatientNotFoundByIdException;
+import com.jsp.pharmassist.utility.AppResponseBuilder;
+import com.jsp.pharmassist.utility.ErrorStructure;
+
+@RestControllerAdvice
+public class PatientExceptionHandler {
+
+	private final AppResponseBuilder responseBuilder;
+
+	public PatientExceptionHandler(AppResponseBuilder responseBuilder) {
+		super();
+		this.responseBuilder = responseBuilder;
+	}
+	
+	@ExceptionHandler(PatientNotFoundByIdException.class)
+	public ResponseEntity<ErrorStructure> handlePatientFoundNotById(PatientNotFoundByIdException ex) {
+		
+		return responseBuilder.error(HttpStatus.NOT_FOUND,ex.getMessage(),"No patients found based on the id");
+	}
+}
