9757399c433fd518eb0fc757a9a24b28194220bf
Rithika0702 <rithumahathi@gmail.com>
2024-11-05 07:32:51 +0530

    Register Patient feature is added

---
+++ src/main/java/com/jsp/pharmassist/controller/PatientController.java
@@ -0,0 +1,35 @@
+package com.jsp.pharmassist.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jsp.pharmassist.requestdtos.PatientRequest;
+import com.jsp.pharmassist.responsedtos.PatientResponse;
+import com.jsp.pharmassist.service.PatientService;
+import com.jsp.pharmassist.utility.AppResponseBuilder;
+import com.jsp.pharmassist.utility.ResponseStructure;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class PatientController {
+	
+	private final PatientService patientService;
+	private final AppResponseBuilder responseBuilder;
+	
+	public PatientController(PatientService patientService, AppResponseBuilder responseBuilder) {
+		super();
+		this.patientService = patientService;
+		this.responseBuilder = responseBuilder;
+	}
+	
+	@PostMapping("/pharmacies/{pharmacyId}/patients")
+	public ResponseEntity<ResponseStructure<PatientResponse>> addPatient(@RequestBody @Valid PatientRequest patientRequest, @PathVariable String pharmacyId) {
+		 PatientResponse response = patientService.addPatient(patientRequest,pharmacyId);
+		 return responseBuilder.success(HttpStatus.CREATED,"Patient registered",response);
+	}	
+}
---
+++ src/main/java/com/jsp/pharmassist/service/PatientService.java
@@ -0,0 +1,42 @@
+package com.jsp.pharmassist.service;
+
+import org.springframework.stereotype.Service;
+
+import com.jsp.pharmassist.entity.Patient;
+import com.jsp.pharmassist.exception.PharmacyNotFoundByIdException;
+import com.jsp.pharmassist.mapper.PatientMapper;
+import com.jsp.pharmassist.repository.PatientRepository;
+import com.jsp.pharmassist.repository.PharmacyRepository;
+import com.jsp.pharmassist.requestdtos.PatientRequest;
+import com.jsp.pharmassist.responsedtos.PatientResponse;
+
+@Service
+public class PatientService {
+	
+	private final PatientRepository patientRepository;
+	private final PharmacyRepository pharmacyRepository;
+	private final PatientMapper patientMapper;
+
+	public PatientService(PatientRepository patientRepository, PharmacyRepository pharmacyRepository,
+			PatientMapper patientMapper) {
+		super();
+		this.patientRepository = patientRepository;
+		this.pharmacyRepository = pharmacyRepository;
+		this.patientMapper = patientMapper;
+	}
+
+	public PatientResponse addPatient( PatientRequest patientRequest, String pharmacyId) {
+
+		return pharmacyRepository.findById(pharmacyId)
+				.map(pharmacy  ->{
+					Patient patient=patientMapper.mapToPatient(patientRequest, new Patient());
+					patient.setPharmacy(pharmacy);
+					pharmacy.getPatients().add(patient);
+					patientRepository.save(patient);
+					return patientMapper.mapToPatientResponse(patient);
+				})
+				.orElseThrow(() ->new PharmacyNotFoundByIdException("Failed to find Pharmacy"));
+                        
+	}
+
+}
